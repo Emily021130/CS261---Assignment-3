fe4657096a251144b95c10455200e785695cf76c
Shuyao zeng <zengs@oregonstate.edu>
2023-07-26 19:08:45 -0700

    Take a try:)

--- queue_sll.py
+++ queue_sll.py
@@ -71,7 +71,7 @@ class Queue:
             self._tail = SLNode(value)
         else:
             self._tail.next = SLNode(value)
-            self._tail = SLNode(value)
+            self._tail = self._tail.next
 
     def dequeue(self) -> object:
         """
